52961ab2974f6866754129d1f704257475fff864
Siddharth Mittal <smittal6@gmail.com>
2017-01-14 22:11:54 +0530

    Assignment 1, Question 1

---
+++ ass1/script1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if (($#!=2))
+	then echo "Not correct number of arguments.Try again."
+	exit 0
+fi
+
+if (($1<$2))
+		then echo "$1 < $2"
+elif (($1==$2))
+		then echo "$1 = $2"
+else
+		echo "$1 > $2"
+fi
+
