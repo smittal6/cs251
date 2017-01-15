10d20f91088a97d337d34510d050aac0a3ac6039
Siddharth Mittal <smittal6@gmail.com>
2017-01-15 16:30:20 +0530

    Additional check for numbers

--- ass1/compare.sh
+++ ass1/compare.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+re="^-?[0-9]+$"
 if (($#!=2));
-	then echo "Not correct number of arguments.Try again."
+	then echo "Not correct number of arguments. Try again."
 	exit 1
 fi
+if  [[ ! $1 =~ $re ]] || [[ ! $2 =~ $re ]];
+	then echo "Please input numbers"
+	exit 1
+fi	
 
 if (($1<$2));
 		then echo "$1 < $2"
