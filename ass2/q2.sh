d77143a9ecd7ed6331acd053537c36ffe2896b1a
Siddharth Mittal <smittal6@gmail.com>
2017-01-22 14:43:13 +0530

    initializing the file

---
+++ ass2/q2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Inputs n,m,d,file f,t and g
+
+#Array for students[indexed at zero], with the value in the array being the day assigned to it
+#array for number(by using percentages) of students on days.
+#array to store the percentages from the file
+#array for keeping track of actually allocated students to days.
+
+n=$1;
+m=$2;
+d=$3;
+file=$4;
+t=$5;
+g=$6;
+
+echo "$1 $2 $3 $5 $6"
