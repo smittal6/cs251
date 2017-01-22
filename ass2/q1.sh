eff61c65c7cfff0061a0ad28cf5ac7b443f0d1b4
Siddharth Mittal <smittal6@gmail.com>
2017-01-22 12:27:50 +0530

    Initial commit for assign 2

---
+++ ass2/q1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash/
+awk '
+BEGIN { FS=",";}
+{
+	if (NF%2==0){
+		for(i=1;i<NF;i=i+2){
+			printf "%s,%s",$(i+1),$i;
+			if (i<NF-1)
+				printf ",";	
+		}	
+	}
+	else{
+			for(i=1;i<NF-1;i=i+2){
+					printf "%s,%s,",$(i+1),$i;
+			}
+			printf "%s",$i;
+	}
+	printf "\n";
+}
+END{ print "done"}' $1
+
