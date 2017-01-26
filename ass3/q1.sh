9c4181ce51344399baec11e828f3926d8d717c70
Siddharth Mittal <smittal6@gmail.com>
2017-01-27 00:42:47 +0530

    First question done

--- ass3/q1.sh
+++ ass3/q1.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-
-#Read the numbers line by line.
-#Store the numbers, find max and min
-#Go over the array, normalize them
-#Output the array
-
-while read line
-do
-        echo -e "$line"
+array=( $(sort $1) )
+#echo "${array[*]}"
+min=${array[0]}
+length=${#array[*]}
+max=${array["$length-1"]}
+#echo "$min $max"
+array1=()
+index=0
+while read line;do
+       array1[index]="$line"
+       let index=index+1
 done < $1
+lindex=$(($length-1))
+for i in $(seq 0 $lindex);do
+        array1[$i]=$(echo "scale=4; (${array1[$i]}-$min)/($max-$min)"| bc -l)
+done
+echo "${array1[*]}"
