4a23f4401a81fe28f10de0beae3fb774487c35af
Siddharth Mittal <smittal6@gmail.com>
2017-01-26 23:46:17 +0530

    Line by line input happening

---
+++ ass3/q1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Read the numbers line by line.
+#Store the numbers, find max and min
+#Go over the array, normalize them
+#Output the array
+
+while read line
+do
+        echo -e "$line"
+done < $1
