a7cbe54b8774a2578ac98260d89d4de119b92010
Siddharth Mittal <smittal6@gmail.com>
2017-01-27 20:54:04 +0530

    added support for reals

--- ass3/q1.sh
+++ ass3/q1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-array=( $(sort $1) )
+array=( $(sort -g $1) )
 #echo "${array[*]}"
 min=${array[0]}
 length=${#array[*]}
@@ -11,8 +11,13 @@ while read line;do
        array1[index]="$line"
        let index=index+1
 done < $1
+#readarray -t array1 < $1
+#echo "${array1[*]}"
 lindex=$(($length-1))
+#echo "$lindex"
+#so uptil now, array1 is correct, lindex is correct. Let us fucking use awk now.
 for i in $(seq 0 $lindex);do
-        array1[$i]=$(echo "scale=4; (${array1[$i]}-$min)/($max-$min)"| bc -l)
+        echo $max $min ${array1[$i]} | awk '{ printf("%.4f\n"),($3-$2)/($1 -$2); exit;}'
 done
-echo "${array1[*]}"
+#echo "${array1[*]}"
+#awk -v max="$max" -v min="$min"
