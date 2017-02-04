2cf5e69422ac6fdbb5fe59822a2fec893946e202
Siddharth Mittal <smittal6@gmail.com>
2017-02-04 22:22:01 +0530

    Question 1 done

--- ass4/q1.m
+++ ass4/q1.m
@@ -1,12 +1,32 @@
+##Clear the workspace
 clear all;
+
+
+##Accepting the input
 n=input("Square root of which number: ");
 k=input("How many random numbers: ");
 
-##Generate k random numbers
+###Checking n,k are positive or not
+if (n<0 | k<0)
+  error ("Please enter non negative values")
+endif
 
+
+
+##Generate k random numbers
 random=rand(1,k)*n;
 
+###This is to calculate the proportion of numbers less than root n
 count=0;
 for i=1:k
-  
+  if ( power(random(1,i),2)<=n)
+    count=count+1;
+  else
+    continue;
+  endif
 end
+
+# sqrt(n)/n is approximately count/sample size.
+
+rootn=(n*count)/k;
+disp(rootn)
