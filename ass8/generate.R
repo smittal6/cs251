c935ca0fea0c48d0a727b05ec7b09325c6387fde
Siddharth Mittal <smittal6@gmail.com>
2017-03-16 22:42:35 +0530

    add script and data files

---
+++ ass8/generate.R
@@ -0,0 +1,7 @@
+base_dir="~/Desktop/coding/cs251/ass8/"
+for(i in 1:10){
+  f=file(paste("~/Desktop/coding/cs251/ass8/uni",i,".txt",sep=""),'w');
+  #normal=rnorm(100,0.5,0.15)
+  u = runif(100,0.0,1.0)
+  cat(u,file=f)
+}
