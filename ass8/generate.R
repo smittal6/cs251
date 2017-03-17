a79b5740f7888bbc25715896f31b25d4ad824a36
Siddharth Mittal <smittal6@gmail.com>
2017-03-17 18:31:54 +0530

    from space separated to newline separated data

--- ass8/generate.R
+++ ass8/generate.R
@@ -3,5 +3,6 @@ for(i in 1:10){
   f=file(paste("~/Desktop/coding/cs251/ass8/uni",i,".txt",sep=""),'w');
   #normal=rnorm(100,0.5,0.15)
   u = runif(100,0.0,1.0)
-  cat(u,file=f)
+  a=cbind(u)
+  cat(a,file=f)
 }
