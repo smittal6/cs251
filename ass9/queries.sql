c9f6b20afb81e211f9aaa818d061662f9d77a003
Siddharth Mittal <smittal6@gmail.com>
2017-03-27 01:50:58 +0530

    removed errors

--- ass9/queries.sql
+++ ass9/queries.sql
@@ -20,21 +20,26 @@ select count(*) from students;
 select distinct count(id) from family; 
 
 .print 'Total: '
-select (
-select count(*) from facstaff where facorstaff="fac"+
-select count(*) from students+
-select distinct count(id) from family+
-select count(*) from facstaff where facorstaff="staff");
+select (select count(*) from facstaff where facorstaff="fac")+(select count(*) from students)+( select distinct count(id) from family)+(select count(*) from facstaff where facorstaff="staff");
+
 /* Query d*/
 .print 'Average number of family members per faculty member'
-select (select distinct count(id) from family where (relid>=1 and relid<=50))/(select count(*) from facstaff where facorstaff="fac");
+select (select distinct count(id) from family where (relid>=1 and relid<=50))*1.0/(select count(*) from facstaff where facorstaff="fac");
 
 /*Query E*/
 
 /*Query f*/
-.print 'Percentage of UG students having an advisor'
+.print 'Percentage of UG students having an advisor: '
 select ((select count(*) from students where ugorpg="ug" and advisor1 is not null)*100.0)/(select count(*) from students where ugorpg="ug");
 
-/*Query g
-.print 'Query G'
-select id from facstaff where (facstaff.id=(select advisor2 from students) and facstaff.department!=(select department from students));*/
+/*Query g*/
+.print 'Number of faculty members advising students from other departments: '
+/*select
+	one.id
+from
+	facstaff one
+	join students two
+	on one.id=two.advisor2
+where
+a.department!=b.department;*/
+select distinct count(advisor2) from students where a2dep is not null and a2dep!=department;
