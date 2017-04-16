d331a104dd82a674490fa1c6cfa9a02b33a4ee5b
Siddharth Mittal <smittal6@gmail.com>
2017-04-17 01:48:20 +0530

    clean code

--- ass12/q2.py
+++ ass12/q2.py
@@ -1,10 +1,8 @@
 input_string = raw_input('Enter the string: ')
 vowels=['a','e','i','o','u','A','E','I','O','U',' ','.','!']
-count=0
 output_string=""
 for s in input_string:
     if s not in vowels:
-        count=count+1
         output_string=output_string+s+"o"+s
     else:
         output_string=output_string+s
